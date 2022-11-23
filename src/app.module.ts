b1db494e6437a74a209c229b4ecb89dcbb9230a5
andrew-yip <andrew.yip22@gmail.com>
2022-11-23 13:13:32 -0800

    errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [AppController], // how you handle incoming requests (send back a response)
   providers: [AppService], // extra services/classes that you can inject into other providers to provide functionality
 })
