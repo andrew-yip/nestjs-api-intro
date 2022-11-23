fd863b32c0ae62dd842e020700b37fcd6cc27a0d
andrew-yip <andrew.yip22@gmail.com>
2022-11-23 14:45:15 -0800

    added get request

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 
 @Controller('products')
@@ -9,5 +9,10 @@ export class ProductsController {
     addProduct(@Body('title') prodTitle: string, @Body('description') prodDesc: string, @Body('price') prodPrice: number) {
         const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
         return { id: generatedId };
+    }
+
+    @Get()
+    getAllProducts() {
+        return this.productsService.getProducts();
     }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,12 +4,18 @@ import { Product } from "./product.model";
 
 @Injectable()
 export class ProductsService {
-    products: Product[] = [];
+    private products: Product[] = [];
 
     insertProduct(title: string, desc: string, price: number) {
         const prodId = new Date().toString(); // temp unique id
         const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
         return prodId;
+    }
+
+    getProducts() {
+        //return this.products; // this is only a pointer
+
+        return [...this.products];
     }
 }
