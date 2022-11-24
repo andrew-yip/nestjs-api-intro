50ad482716785b56a15309d1f0d62edb4e47119f
andrew-yip <andrew.yip22@gmail.com>
2022-11-23 16:24:19 -0800

    completed crud functionality to nest app

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, PreconditionFailedException, Patch, Delete } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 
 @Controller('products')
@@ -6,8 +6,16 @@ export class ProductsController {
     constructor(private readonly productsService: ProductsService) { }
 
     @Post()
-    addProduct(@Body('title') prodTitle: string, @Body('description') prodDesc: string, @Body('price') prodPrice: number) {
-        const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+    addProduct(
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number,
+    ) {
+        const generatedId = this.productsService.insertProduct(
+            prodTitle,
+            prodDesc,
+            prodPrice,
+        );
         return { id: generatedId };
     }
 
@@ -15,4 +23,22 @@ export class ProductsController {
     getAllProducts() {
         return this.productsService.getProducts();
     }
+
+    @Get(':id')
+    getProduct(@Param('id') prodId: string,) {
+        return this.productsService.getSingleProduct(prodId);
+    }
+
+    @Patch(':id')
+    updateProduct(@Param('id') prodId: string, @Body('title') prodTitle: string, @Body('description') prodDesc: string, @Body('price') prodPrice: number) {
+        this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+        return null;
+    }
+
+    @Delete(':id')
+    removeProduct(@Param('id') prodId: string,) {
+        this.productsService.deleteProduct(prodId);
+        return null;
+    }
+
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 
 import { Product } from "./product.model";
 
@@ -7,15 +7,49 @@ export class ProductsService {
     private products: Product[] = [];
 
     insertProduct(title: string, desc: string, price: number) {
-        const prodId = new Date().toString(); // temp unique id
+        const prodId = Math.random().toString();
         const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
         return prodId;
+        //return newProduct;
     }
 
     getProducts() {
         //return this.products; // this is only a pointer
+        return [...this.products]; // spread operator and adding elements to new array from products array
+    }
 
-        return [...this.products];
+    getSingleProduct(productId: string) {
+        const product = this.findProduct(productId)[0];
+        return { ...product };
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = { ...product }; // old product in new object
+        if (title) {
+            updatedProduct.title = title;
+        }
+        if (desc) {
+            updatedProduct.description = desc;
+        }
+        if (price) {
+            updatedProduct.price = price;
+        }
+        this.products[index] = updatedProduct;
+    }
+
+    deleteProduct(prodId: string) {
+        const index = this.findProduct(prodId)[1];
+        this.products.splice(index, 1);
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex(prod => prod.id === id);
+        const product = this.products[productIndex];
+        if (!product) {
+            throw new NotFoundException('Could not find product.');
+        }
+        return [product, productIndex];
     }
 }
